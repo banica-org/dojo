5a3a034129f69a5eb0b2c642ff319564c4f9b5a9
stef2georg <stefan.georgiev91@gmail.com>
2021-03-04 13:54:36 +0200

    Cleanup

--- notifications/src/test/java/com/dojo/notifications/NotifierApplicationTests.java
+++ notifications/src/test/java/com/dojo/notifications/NotifierApplicationTests.java
@@ -1,13 +1,6 @@
 package com.dojo.notifications;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class NotifierApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
