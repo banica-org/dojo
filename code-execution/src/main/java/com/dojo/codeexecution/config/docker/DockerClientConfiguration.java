5f51972da4f9269cc3f3dbf6c6b3c908e8b00efc
Georgi Ivanov1 <georgi_ivanov1@epam.com>
2021-04-01 15:25:51 +0300

    Move all bean configurations in single class.

--- code-execution/src/main/java/com/dojo/codeexecution/config/docker/DockerClientConfiguration.java
+++ code-execution/src/main/java/com/dojo/codeexecution/config/docker/DockerClientConfiguration.java
@@ -1,10 +1,11 @@
 package com.dojo.codeexecution.config.docker;
 
 import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientConfig;
 import com.github.dockerjava.core.DockerClientImpl;
+import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
 import com.github.dockerjava.transport.DockerHttpClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,7 +13,19 @@ import org.springframework.context.annotation.Configuration;
 public class DockerClientConfiguration {
 
     @Bean
-    public DockerClient createDockerClient(DockerClientConfig clientConfig, DockerHttpClient httpClient){
-        return DockerClientImpl.getInstance(clientConfig, httpClient);
+    public DockerClient createDockerClient() {
+        return DockerClientImpl.getInstance(dockerDefaultClientConfig(), httpClient());
+    }
+
+    @Bean
+    DockerClientConfig dockerDefaultClientConfig() {
+        return DefaultDockerClientConfig.createDefaultConfigBuilder().build();
+    }
+
+    @Bean
+    DockerHttpClient httpClient() {
+        return new ApacheDockerHttpClient.Builder()
+                .dockerHost(dockerDefaultClientConfig().getDockerHost())
+                .build();
     }
 }
