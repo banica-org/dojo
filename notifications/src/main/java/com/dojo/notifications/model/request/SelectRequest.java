64dbca2a7c9e1a80fb6bfd3d3aa4365fa08a0c9c
Kaloyan Dutsolov <kaloyan_dutsolov@epam.com>
2021-04-01 16:19:09 +0300

    Add Service and repo for the H2 DB

---
+++ notifications/src/main/java/com/dojo/notifications/model/request/SelectRequest.java
@@ -0,0 +1,37 @@
+package com.dojo.notifications.model.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name="SELECT_REQUESTS")
+public class SelectRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    int ID;
+
+    @Column(name = "QUERY")
+    String QUERY;
+
+    @Column(name = "NOTIFICATION_TYPE")
+    String NOTIFICATION_TYPE;
+
+    @Column(name = "MESSAGE")
+    String MESSAGE;
+
+    @Column(name = "QUANTIFIER")
+    String QUANTIFIER;
+}
