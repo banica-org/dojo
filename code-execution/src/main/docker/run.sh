b5baf68525c2460b8b80a577cf7ba790c079a066
Georgi Ivanov1 <georgi_ivanov1@epam.com>
2021-04-12 22:23:57 +0300

    Add github username as testing main method argument.

--- code-execution/src/main/docker/run.sh
+++ code-execution/src/main/docker/run.sh
@@ -5,4 +5,4 @@ git clone --depth 1 --no-tags https://$1:$2@github.com/$3/$4
 #cd test-child && mvn package && cd ..
 cd $4 && mvn -DskipTests package && cd ..
 #java -cp test-parent-1.0-SNAPSHOT-jar-with-dependencies.jar:test-child/target/test-child-1.0-SNAPSHOT.jar test.parent.Main
-java -cp "${4}"/target/"${4}"-1.0-SNAPSHOT.jar:"${5}"-1.0-SNAPSHOT-solution-tests.jar TestMain
+java -cp "${4}"/target/"${4}"-1.0-SNAPSHOT.jar:"${5}"-1.0-SNAPSHOT-solution-tests.jar TestMain $3
