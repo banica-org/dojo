502b1a408aa2cb0097559fd9c098b51f8605bd76
Elitsa Marinova <Elitsa_Marinova@epam.com>
2021-06-14 09:29:30 +0300

    refactor

--- performance-tests/src/main/java/com/dojo/performancetests/Main.java
+++ performance-tests/src/main/java/com/dojo/performancetests/Main.java
@@ -1,8 +1,10 @@
 package com.dojo.performancetests;
 
 public class Main {
-
     public static void main(String[] args) {
-
+        //TODO
+        //Add valid user data in csv file
+        //Add valid mail details and slack token
+        //Enable all controllers in test plan
     }
 }
