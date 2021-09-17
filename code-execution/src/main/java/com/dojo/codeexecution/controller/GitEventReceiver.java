9a22f8ff080a6393f699f60aef310a605df6cc9e
Nikola_Nguen <nikola.nguen@yahoo.com>
2021-09-17 18:16:42 +0300

    Returned the implementation to the one which is in master, since it works okay.

--- code-execution/src/main/java/com/dojo/codeexecution/controller/GitEventReceiver.java
+++ code-execution/src/main/java/com/dojo/codeexecution/controller/GitEventReceiver.java
@@ -1,7 +1,7 @@
 package com.dojo.codeexecution.controller;
 
 import com.dojo.codeexecution.service.DockerService;
-import com.dojo.codeexecution.util.GithubPushEventManager;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,9 +11,10 @@ import java.util.Map;
 
 @RestController
 public class GitEventReceiver {
-
-    @Autowired
-    GithubPushEventManager githubPushEventManager;
+    public static final String PAYLOAD_FIELD = "repository";
+    public static final String IMAGE_TAG_KEY = "name";
+    public static final String REPO_KEY = "full_name";
+    private static final String REPO_PREFIX = "gamified-hiring-";
 
     @Autowired
     DockerService dockerService;
@@ -27,7 +28,12 @@ public class GitEventReceiver {
     //Currently not able to trigger the webhook which calls this endpoint from github
     @GetMapping(path = "/run")
     public String runContainer(@RequestBody Map<String, Object> payload) {
-        return githubPushEventManager.executeRunContainer(dockerService, payload);
+        Object imageTag = new JSONObject(payload).getJSONObject(PAYLOAD_FIELD)
+                .get(IMAGE_TAG_KEY);
+        Object username = new JSONObject(payload).getJSONObject(PAYLOAD_FIELD)
+                .get(REPO_KEY);
+        dockerService.runContainer((String) imageTag, ((String) username).split("/")[1].replace(REPO_PREFIX, ""));
+        return "OK";
     }
 }
 
