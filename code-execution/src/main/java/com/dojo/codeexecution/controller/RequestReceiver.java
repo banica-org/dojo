fa919ef84a91f33148d61d255f228dc98df395c3
Nikola_Nguen <nikola.nguen@yahoo.com>
2021-11-15 18:57:58 +0200

    Removed unnecessary code.

--- code-execution/src/main/java/com/dojo/codeexecution/controller/RequestReceiver.java
+++ code-execution/src/main/java/com/dojo/codeexecution/controller/RequestReceiver.java
@@ -1,11 +1,8 @@
 package com.dojo.codeexecution.controller;
 
 import com.dojo.codeexecution.service.GitManager;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +10,6 @@ import java.io.IOException;
 
 @RestController
 public class RequestReceiver {
-
-    private final static Logger LOGGER = LoggerFactory.getLogger(RequestReceiver.class);
 
     @Autowired
     private GitManager gitManager;
