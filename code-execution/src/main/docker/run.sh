bf956c0f4d377373ae370c72e0fb0e15db47a03c
Kaloyan Dutsolov <kaloyan_dutsolov@epam.com>
2022-02-23 11:46:33 +0200

    Add SandBoxMain run and if fail doesn't run TestMain

--- code-execution/src/main/docker/run.sh
+++ code-execution/src/main/docker/run.sh
@@ -8,6 +8,8 @@ export MAVEN_OPTS="-XX:+TieredCompilation -XX:TieredStopAtLevel=1"
 cd $4 && echo build-log-separator && \
 mvn -Dmaven.repo.local=/usr/share/maven/ref/repository -Dmaven.test.skip -DskipTests package -o && \
 echo build-log-separator && \
-cd .. && java -cp "${4}"/target/"${4}"-1.0-SNAPSHOT.jar:"${5}"-1.0-SNAPSHOT-solution-tests.jar TestMain $6
+set -e
+cd .. && java -cp "${4}"/target/"${4}"-1.0-SNAPSHOT.jar:"${5}"-1.0-SNAPSHOT-solution-tests.jar SandBoxMain $6 -eq 1
+java -cp "${4}"/target/"${4}"-1.0-SNAPSHOT.jar:"${5}"-1.0-SNAPSHOT-solution-tests.jar TestMain $6
 #java -cp test-parent-1.0-SNAPSHOT-jar-with-dependencies.jar:test-child/target/test-child-1.0-SNAPSHOT.jar test.parent.Main
 echo build-log-separator
